f133b553718912f0fc6754bf18396f7e8963f958
Петр Балдаев <petrbaldaev@MacBook-Air-Petr.local>
2022-11-24 13:17:27 +0600

    docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 sleep 3
 python manage.py migrate
 python manage.py createcachetable
+python manage.py loaddata subjects.json
 python manage.py collectstatic  --noinput
 gunicorn project.wsgi:application --bind 0.0.0.0:8000
 exec "$@"
