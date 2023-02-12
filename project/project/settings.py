99b9d5f100c87cc98e212da49846e471dfff057d
Петр Балдаев <petrbaldaev@MacBook-Air-Petr.local>
2023-02-13 00:08:37 +0600

    error correction

--- project/project/settings.py
+++ project/project/settings.py
@@ -59,12 +59,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
